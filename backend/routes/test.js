c45db447a119bf6b1332e25c1dc6335535a2db51
rajahwu <vmradford@gmail.com>
2023-02-20 17:09:23 -0500

    add group images seeder

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -3,30 +3,15 @@ const { utils } = require('../db/data_generator');
 const router = express.Router();
 
 const dataGen = require('../db/data_generator')
-const { User, Group } = require('../db/models')
-
-async function genMembership(num) {
-    const memberships = []
-    const users = await User.findAll()
-    const groups = await Group.findAll()
-    const userIds = dataGen.utils.getIds(users)
-    const groupIds = dataGen.utils.getIds(groups)
-    for( let i = 0; i < num; i++ ) {
-        const membership = new dataGen.membership(dataGen.utils, {
-            userId: userIds[dataGen.utils.getRandom(userIds.length - 1)],
-            groupId: groupIds[dataGen.utils.getRandom(groupIds.length - 1)]
-        })
-        memberships.push(membership)
-    }
-    return memberships
-}
-
+const { User, Group, GroupImage } = require('../db/models')
 
 
 
 router.get('/', async (req, res) => {
-    const memberships = await genMembership(5)
-    res.json(memberships)
+    const images = await Group.findAll({
+        include: [{model: GroupImage}]
+    })
+    res.json(images)
 })
 
 module.exports = router
