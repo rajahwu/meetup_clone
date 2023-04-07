e7a8a2f37f0738790f29e72aee8a2bc10383415b
rajahwu <vmradford@gmail.com>
2023-04-07 17:01:50 -0400

    create model context

---
+++ frontend/src/context/Modal.js
@@ -0,0 +1,52 @@
+import React, { useRef, useState, useContext } from "react";
+import ReactDOM from "react-dom";
+import ModalCSS from "./Modal.module.css"
+
+const ModalContext = React.createContext();
+
+export function ModalProvider({ children }) {
+  const modalRef = useRef();
+  const [modalContent, setModalContent] = useState(null);
+  const [onModalClose, setOnModalClose] = useState(null);
+
+  const closeModel = () => {
+    setModalContent(null);
+    if (typeof onModalClose === "function") {
+      setOnModalClose(null);
+      onModalClose();
+    }
+  };
+
+  const contextValue = {
+    modalRef,
+    modalContent,
+    setModalContent,
+    setOnModalClose,
+    closeModel,
+  };
+
+  return (
+    <>
+      <ModalContext.Provider value={contextValue}>
+        {children}
+      </ModalContext.Provider>
+      <div ref={modalRef} />
+    </>
+  );
+}
+
+export function Modal() {
+  const { modalRef, modalContent, closeModel } = useContext(ModalContext);
+
+  if (!modalRef || !modalRef.current || !modalContent) return null;
+
+  return ReactDOM.createPortal(
+    <div id="modal" className={ModalCSS.modal}>
+      <div id="modal-background" className={ModalCSS.background} onClick={closeModel} />
+      <div id="modal-content" className={ModalCSS.content}>{modalContent}</div>
+    </div>,
+    modalRef.current
+  );
+};
+
+export const useModal = () => useContext(ModalContext)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider as ReduxProvider } from "react-redux";
+
 import "./index.css";
+
+import ReactDOM from "react-dom/client";
+import { Provider as ReduxProvider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ModalProvider, Modal } from "./context/Modal";
 import App from "./App";
+
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
@@ -20,16 +24,19 @@ if (process.env.NODE_ENV !== "production") {
 
 function Root() {
   return (
-    <ReduxProvider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ReduxProvider>
+    <ModalProvider>
+      <ReduxProvider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </ReduxProvider>
+    </ModalProvider>
   );
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Root />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
