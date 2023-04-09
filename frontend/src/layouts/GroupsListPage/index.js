f10e6e48c16c0611de104445b356b198bdf573af
rajahwu <vmradford@gmail.com>
2023-04-09 12:43:14 -0400

    add event groups header caption

--- frontend/src/layouts/GroupsListPage/index.js
+++ frontend/src/layouts/GroupsListPage/index.js
@@ -5,7 +5,7 @@ import { getAllGroups } from "../../store/groups";
 export default function GroupsListPage() {
   const dispatch = useDispatch();
   const groups = useSelector((state) => state?.groups);
-  console.log(groups["1"]);
+  // console.log(groups["1"]);
 
   useEffect(() => {
     dispatch(getAllGroups());
