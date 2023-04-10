b994d5dd654be5423cac263f3c0218725330b1a7
rajahwu <vmradford@gmail.com>
2023-04-10 19:53:11 -0400

    add event to group details page

--- frontend/src/components/GroupDetailCard/index.js
+++ frontend/src/components/GroupDetailCard/index.js
@@ -1,24 +1,56 @@
+import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import CardImage from "../CardImage";
+import GroupEventCard from "../GroupEventCard";
+import { getGroupEvents } from "../../store/events";
 
 export default function GroupDetailCard({ group, children }) {
+  const dispatch = useDispatch();
+  const events = useSelector((state) => state.events);
+  console.log(events);
+  useEffect(() => {
+    dispatch(getGroupEvents(group?.id));
+  }, [dispatch]);
+
   if (group)
     return (
-      <div style={{ display: "flex" }}>
-        <CardImage imageWidth="300px" imageHeight="250px" />
-        <div>
-          <h2>{group.name}</h2>
-          <p>
-            {group.city}, {group.state}
-          </p>
+      <>
+        <NavLink to="/group">Groups</NavLink>
+        <div style={{ display: "flex" }}>
+          <CardImage imageWidth="300px" imageHeight="250px" />
           <div>
-            <p>## events</p>
-            <p>{group.private ? "Private" : "Public"}</p>
+            <h2>{group.name}</h2>
+            <p>
+              {group.city}, {group.state}
+            </p>
+            <div>
+              <p>## events</p>
+              <p>{group.private ? "Private" : "Public"}</p>
+            </div>
+            <p>
+              Organized by {group.Organizer?.firstName}{" "}
+              {group.Orginzer?.lastName}
+            </p>
+            {children}
           </div>
-          <p>
-            Organized by {group.Organizer?.firstName} {group.Orginzer?.lastName}
-          </p>
-          {children}
         </div>
-      </div>
+        <div>
+          <h2>What we're about</h2>
+          <p>{group.about}</p>
+        </div>
+        {events && Object.values(events).map(event => (
+          <GroupEventCard 
+            key={event.id}
+            name={event.name}
+            description="Not in data"
+            city={event.city}
+            state={event.state}
+            type={event.type}
+           >
+            <CardImage />
+           </GroupEventCard>
+        ))}
+      </>
     );
 }
--- frontend/src/store/events.js
+++ frontend/src/store/events.js
@@ -2,6 +2,7 @@ import { csrfFetch } from "./csrf";
 
 const LOAD_EVENTS = "groups/loadEvents";
 const RECEIVE_EVENT = "groups/receiveEvent";
+const RECEIVE_EVENTS = "groups/receiveEvents";
 const UPDATE_EVENT = "groups/updateEvent";
 const REMOVE_EVENT = "groups/removeEvent";
 
@@ -18,16 +19,30 @@ export const getAllEvents = () => async (dispatch) => {
   }
 };
 
+// export const recieveGroupEvents = (events) => ({
+//   type: RECEIVE_EVENTS,
+//   payload: events
+
+// })
+
+export const getGroupEvents = (groupId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/groups/${groupId}/events`)
+  const events = await response.json()
+  if (response.ok) {
+    return dispatch(loadEvents(events))
+  }
+}
+
 const eventsReducer = (state = {}, action) => {
   switch (action.type) {
     case LOAD_EVENTS: {
-      const eventState = {};
+      const eventState = { ...state };
       action.payload.Events.forEach((event) => {
         eventState[event.id] = event;
       });
       return eventState;
     }
-
+   
     default:
       return state;
   }
