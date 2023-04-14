0985630fa77041e39640ea9937cafc03164bcd51
rajahwu <vmradford@gmail.com>
2023-04-14 18:58:04 -0400

    add images to detail cards

--- frontend/src/components/EventDetailCard/index.js
+++ frontend/src/components/EventDetailCard/index.js
@@ -1,7 +1,13 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { CardImage } from "../../components";
+import { checkForImage } from "../../utils/checkForImage";
+const defaultImages = ["https://images.unsplash.com/photo-1519750157634-b6d493a0f77c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"]
+
+
 
 export default function EventDetailCard({ event, children }) {
+  const group = useSelector(state => state.groups.currentGroup)
     if(!event.id) return null
   return (
     <>
@@ -15,16 +21,18 @@ export default function EventDetailCard({ event, children }) {
         </div>
 
         <div className="main-div" style={{display: "flex", border: "3px solid pink"}}>
-          <CardImage imageHeight="200px" imageWidth="300px" />
+          <CardImage imageHeight="200px" imageWidth="300px" imageUrl={checkForImage([event.previwImage], defaultImages)}/>
             
           <div>
-            <CardImage imageWidth="50px" imageHeight="50px" />
+            <CardImage imageWidth="50px" imageHeight="50px"
+             imageUrl={checkForImage([group?.GroupImages], defaultImages)}
+
+             />
             <p> {event.Group?.name}</p>
             <p>{event.Group?.type}</p>
           </div>
 
           <div>
-          {console.log("event details page event", event)}
             <p>{event.startDate}</p>
             <p>{event.endDate}</p>
             <p>{event.price}</p>
