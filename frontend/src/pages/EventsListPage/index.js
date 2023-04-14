da48e9a21048b9f20dd1d646679669b1dbbe4a98
rajahwu <vmradford@gmail.com>
2023-04-14 18:38:19 -0400

    add image to event list page

--- frontend/src/pages/EventsListPage/index.js
+++ frontend/src/pages/EventsListPage/index.js
@@ -2,6 +2,8 @@ import { useHistory } from "react-router-dom";
 import { CardImage, GroupEventCard } from "../../components";
 import {Link} from "react-router-dom"
 import { useGetAll } from "../../hooks";
+import { checkForImage } from "../../utils/checkForImage";
+const defaultImages = ["https://images.unsplash.com/photo-1519750157634-b6d493a0f77c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"]
 
 export default function EventsListPage() {
   const events = useGetAll("events").allEvents;
@@ -22,7 +24,7 @@ export default function EventsListPage() {
               state={event.Venue ? event.Venue.state : <Link to={`/events/${event.id}`}>Location</Link>}
               type={event.type}
             >
-              <CardImage imageWidth={100} imageHeight={100} />
+              <CardImage imageWidth={100} imageHeight={100} imageUrl={checkForImage(event, defaultImages)}/>
             </GroupEventCard>
             <hr />
           </div>
