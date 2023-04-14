f88cbd5aeef8b2577ac13a4cef0591c025bb39d3
rajahwu <vmradford@gmail.com>
2023-04-14 12:29:12 -0400

    add sign up to cta

--- frontend/src/components/CallToAction/index.js
+++ frontend/src/components/CallToAction/index.js
@@ -1,7 +1,22 @@
-const CallToAction = ({ StyleSheet }) => (
-    <section className={StyleSheet["cta-container"]}>
-      <button className={StyleSheet["cta-btn"]}>Join Meetup</button>
-    </section>
-  );
+import { useSelector } from "react-redux";
+import OpenModelButton from "../Navigation/OpenModalButton";
+import { LoginFormModal } from "../session";
 
-  export default CallToAction
+const CallToAction = ({ StyleSheet }) => {
+  const sessisonUser = useSelector((state) => state.session.user);
+  console.log(!sessisonUser);
+  return (
+    !sessisonUser && (
+      <section className={StyleSheet["cta-container"]}>
+        <OpenModelButton
+          buttonText={
+            <button className={StyleSheet["cta-btn"]}>Join Meetup</button>
+          }
+          modalComponent={<LoginFormModal />}
+        />
+      </section>
+    )
+  );
+};
+
+export default CallToAction;
