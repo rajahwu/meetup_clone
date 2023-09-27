2c8518356bc3ee4ee55717e2c2a3a44f9c27855e
rajahwu <vmradford@gmail.com>
2023-09-27 14:45:09 -0400

    style login form

--- frontend/src/components/session/LoginFormModal/index.js
+++ frontend/src/components/session/LoginFormModal/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import * as sessionActions from "../../../store/session";
 import { useDispatch } from "react-redux";
-import {useHistory} from "react-router-dom"
+import { useHistory } from "react-router-dom"
 import { useModal } from "../../../context/Modal";
 import { useSetModalClass } from "../../../hooks";
 
@@ -28,11 +28,11 @@ export default function LoginFormModel() {
   };
 
   return (
-    <div className="">
-      <h1>Log In</h1>
+    <div className="w-full h-full max-w-xs card flex items-center justify-center my-5">
+      <h1 className="card-title text-center">Log In</h1>
       {errors.credential && <p>{errors.credential}</p>}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="credential">
+      <form className="rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="credential">
           <input
             id="credential"
             type="text"
@@ -42,7 +42,7 @@ export default function LoginFormModel() {
             required
           />
         </label>
-        <label htmlFor="password">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
           <input
             id="password"
             type="password"
@@ -52,15 +52,17 @@ export default function LoginFormModel() {
             required
           />
         </label>
+
         <button
+          className="mr-5 bg-primary hover:bg-blue-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="submit"
-          className=""
           disabled={credential.length < 4 || password.length < 6}
         >
           Log In
         </button>
 
         <button
+          className="ml-5 bg-primary hover:bg-blue-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="button"
           onClick={() => {
             const demoUser = {
