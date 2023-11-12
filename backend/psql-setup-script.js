2a739ebdad32cf869b735640073e6ba16315e453
rajahwu <vmradford@gmail.com>
2023-11-11 21:03:51 -0500

    add loggs to build script

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,8 +1,11 @@
 const { sequelize } = require('./db/models');
 
+console.log('Attempting to connect to the database...');
 sequelize
   .showAllSchemas({ logging: false })
   .then(async (data) => {
+    console.log('Connected to the database successfully.');
+
     if (!data.includes(process.env.SCHEMA)) {
       console.log(`Schema ${process.env.SCHEMA} not found. Creating...`);
       await sequelize.createSchema(process.env.SCHEMA);
